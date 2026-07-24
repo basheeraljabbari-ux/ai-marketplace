26448145ca3944bebec86257e7b045e3af6d25fa
basheeraljabbari-ux <basheeraljabbari@gmail.com>
2026-07-24 20:17:35 +1000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,8 +33,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] if settings.DEBUG else [],  # 👈 حدد دومينات axiscore/marketplace الفعلية بالإنتاج
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
